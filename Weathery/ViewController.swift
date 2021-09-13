1e4601d4a8fd36b5171644a8ef694e9abb2032d7
Furkan Ergün <furkan.ergun@ogr.sakarya.edu.tr>
2021-09-13 11:09:33 +0300

    Initial Commit

---
+++ Weathery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weathery
+//
+//  Created by Furkan Ergün on 13.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
