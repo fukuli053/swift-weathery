61e6b7c05a8f65acdf7bbc1b8fb7bf236de10084
Furkan Ergün <furkan.ergun@ogr.sakarya.edu.tr>
2021-09-13 11:52:05 +0300

    Background and Dark mode added

--- Weathery/ViewController.swift
+++ Weathery/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let backgroundView = UIImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        style()
+        layout()
     }
-
 
 }
 
+extension ViewController {
+    
+    func style(){
+        backgroundView.translatesAutoresizingMaskIntoConstraints = false
+        backgroundView.image = UIImage(named: "background")
+        backgroundView.contentMode = .scaleAspectFill
+    }
+    
+    func layout(){
+        view.addSubview(backgroundView)
+        
+        NSLayoutConstraint.activate([
+            backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+}
